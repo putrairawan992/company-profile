dae0ff06ea720312b9facc9da00c6c0be6c9a245
putra992 <putrairawan.umrahajj@gmail.com>
2018-07-22 14:48:22 +0700

    fix add

--- src/App.js
+++ src/App.js
@@ -8,12 +8,7 @@ class App extends Component {
     return(
       <div>
         <Header />
-          {/* <Card
-          imageLink="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164c0bca190%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164c0bca190%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.203125%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-          CardsTitle="Card title"
-          CardsText="Some quick example text to build on the card title and make up the bulk of the card's content."
-          btnLink="Test"
-           /> */}
+    
           <Jumbotron>
             <h1 className="display-3">Hello, world!</h1>
             <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
